80e894576e244feee9e04fd4c521089b87d46bbc
lucassid <lucassidpacheco@gmail.com>
2017-08-24 12:46:56 -0300

    noise generation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from scipy.io import wavfile
 
+#audio we are going to test
 filename = "audios/Für Elise (original).wav"
 
+#can we read it?
 fs, data = wavfile.read(filename)
+
+assert (fs == 44100)
--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-#not sure if we have something to test already
+from scipy.io import wavfile
+import numpy as np
+
+#audio we are going to test
+filename = "audios/Für Elise (original).wav"
+noisyFileName = "noisy.wav"
+#can we read it?
+fs, data = wavfile.read(filename)
+assert (fs == 44100)
+print(abs(data))
+dataNoisy = data + np.random.standard_normal((len(data),2))
+
+wavfile.write(noisyFileName, fs, dataNoisy)
+#
+print(data - dataNoisy)

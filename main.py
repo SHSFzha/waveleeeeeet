b283e49eca78c0f0544c782e6be2b5906335867f
lucassid <lucassidpacheco@gmail.com>
2017-08-27 15:29:01 -0300

    refactoring

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from scikits.audiolab import wavread, wavwrite
 from tools import functions
 
-data2, fs2, enc2 = wavread("audios/noise.waf")
-data1, fs1, enc1 = wavread("audios/Fur Elise (original).wav")
-
-#both tracks must have same encoding and sampling frequency
-assert fs1 == fs2
-assert enc1 == enc2
-
-#write the ruslt do an other audio file
-originalArray = 1 * data1.T[0] #just to scale the arrays faster
-noisyArray = 1 * data2
-
-result = originalArray + noisyArray
-wavwrite(result, 'result.wav', fs=fs1)
-
-functions.snrCalculation(originalArray, noisyArray)
+originalArray, noisyArray = functions.noiseMixer(0)
+print "snr estimado: " + str(functions.snrCalculation(originalArray, noisyArray)) + " db"
--- tools/functions.py
+++ tools/functions.py
@@ -1,4 +1,30 @@
 import numpy as np
+from scikits.audiolab import wavread, wavwrite
+
+def noiseMixer(outputSnr):
+    # both tracks must have same encoding and sampling frequency
+    data2, fs2, enc2 = wavread("audios/noise.waf")
+    data1, fs1, enc1 = wavread("audios/Fur Elise (original).wav")
+    assert fs1 == fs2
+    assert enc1 == enc2
+
+    # write the result do an other audio file
+    # just to scale the arrays faster
+    originalArray = 1 * data1.T[0]  # this one has two channels, I'm taking just the first
+
+    if outputSnr == 10:
+        noisyArray = 0.015 * data2
+    elif outputSnr == 0:
+        noisyArray = 0.15 * data2
+    elif outputSnr == -10:
+        noisyArray = 0.0475 * data2
+    else:
+        raise ValueError('Unexpected value passed')
+
+    result = originalArray + noisyArray
+    wavwrite(result, 'result.wav', fs=fs1)
+
+    return originalArray, noisyArray
 
 def snrCalculation(original, noise):
     avgPower1 = 0
@@ -7,8 +33,8 @@ def snrCalculation(original, noise):
         avgPower1 += i ** 2
     for i in noise:
         avgPower2 += i ** 2
-    snr = np.log10(avgPower1 / len(original) / (avgPower2 / len(noise)))
+    snr = 10 * np.log10(avgPower1 / len(original) / (avgPower2 / len(noise)))
 
-    print "SNR estimado: " + str(10 * snr)
+    return snr
 
 
